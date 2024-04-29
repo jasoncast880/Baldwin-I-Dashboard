d62013d5a74e4fda0277137d114a6da570de326e
jasoncast880 <jasoncast880@gmail.com>
2024-04-29 12:41:21 -0400

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => activePage = index"
+  ></navbar>
+
+  <page-viewer
+    v-if="pages.length > 0"
+    :page="pages[activePage]"
+  ></page-viewer>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+import PageViewer from './components/PageViewer.vue'
+//import BigScroll from './components/BigScroll.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    PageViewer,
+    //BigScroll
+  },
+  created() {
+    this.getPages();
+  },
+  data() {
+    return {
+      activePage: 0,
+      pages: []
+    }
+  },
+  methods: {
+    async getPages() {
+      let res = await fetch('pages.json');
+      let data = await res.json();
+
+      this.pages = data;
+    },
+    /*
+    pageCreated(pageObj){
+      this.pages.push(pageObj);
+    }
+    */
   }
 }
 </script>
 
 <style>
+/*
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +58,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+*/
 </style>
---
+++ src/components/BigScroll.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1> Jobs </h1>
+  <div v-for = "job in jobs" :key="job.id" class="job">
+    <h2>{{ job.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+      .then((res)=>res.json())
+      .then(data => this.jobs =data)
+      .catch(err => console.log(err.message))
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
